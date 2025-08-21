a614d97e49b2f407be3e823cec9683863d894c0a
예찬 주 <jooyc135@DESKTOP-6DCVU9O.localdomain>
2025-08-21 19:02:33 +0900

    fix:auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -33,3 +33,32 @@ export const checkGroupPassword = async (req, res, next) => {
     res.status(500).json({ error: { message: 'Database error.' } });
   }
 };
+
+// auth.js << 비밀번호 등 검사해야하는거를 여기에 넣으면 좋겠다
+export const checkGroupUser = async (req, res, next) => {
+  const { groupId } = req.params;
+  const { authorNickname, authorPassword, nickname, password } = req.body;
+  const pwd = password ?? authorPassword;
+  const nick = nickname ?? authorNickname;
+
+  try {
+    const groupUser = await prisma.groupUser.findFirst({
+      where: {
+        group_id: parseInt(groupId),
+        nickname: nick,
+      },
+    });
+    if (!groupUser) {
+      return res.status(401).json({ message: '등록된 사용자가 아닙니다.' });
+    }
+    if (groupUser.password !== pwd) {
+      return res.status(401).json({ message: '비밀번호가 일치하지 않습니다.' });
+    }
+    console.log(groupUser);
+    next();
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: '서버 오류가 발생했습니다.' });
+  }
+};
+
